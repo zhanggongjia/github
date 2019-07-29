9d74bf3589d30bebcdb12555ecb0e9c369a77954
张先生 <269702105@qq.com>
2019-07-30 00:35:52 +0800

    react

---
+++ my-project/src/components/Hearder.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../common/css/App.css";
+import { Router, Route, Link } from 'react-router'
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <ul>
+          <li>
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
